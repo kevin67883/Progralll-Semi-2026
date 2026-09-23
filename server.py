601b6e234bd87ae46368343308d04c1ff69dc32e
Kevin Campos <kevin67883@users.noreply.github.com>
2026-09-23 12:02:37 -0600

    Se creo la busqueda y eliminar, modificar clientes

--- server.py
+++ server.py
@@ -1,12 +1,27 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from urllib import parse 
 from urllib.parse import urlparse, parse_qs
+import crud_clientes
 
 import json
 
 port = 3000
+crudClientes = crud_clientes.crud_clientes()
 
 class miServidor(SimpleHTTPRequestHandler):
+    def do_POST(self):
+        longitud = int(self.headers['Content-Length'])
+        datos = self.rfile.read(longitud)
+        datos = datos.decode("utf-8")
+        datos = parse.unquote(datos)
+        datos = json.loads(datos)
+        respuesta = {'msg': crudClientes.administrar(datos)}
+
+        self.send_response(200)
+        self.send_header("Content-type","application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(datos).encode("utf-8"))
+
     def do_GET(self):
         urlParse = urlparse(self.path)
         qs = parse_qs(urlParse.query)
@@ -15,9 +30,9 @@ class miServidor(SimpleHTTPRequestHandler):
             saludo = qs["nombre"][0] + " bienvenido a Python"
             
             self.send_response(200)
-            self.send_header("Content-type","text/html")
+            self.send_header("Content-type","text/json")
             self.end_headers()
-            self.wfile.write(saludo.encode("utf-8"))
+            self.wfile.write(json.dumps().encode("utf-8"))
 
         if self.path == "/":
             self.path = "/index.html"
